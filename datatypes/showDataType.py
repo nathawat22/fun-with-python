65d1b88ccbe7f0a326c8a4024213b32755c7236d
nathawat22 <59749844+nathawat22@users.noreply.github.com>
2020-05-13 14:50:37 -0400

    data types

---
+++ datatypes/showDataType.py
@@ -0,0 +1,6 @@
+num = 12
+price = 2.48
+name = "Jack"
+print("Data type of num variable is ", type(num))
+print("Data type of price variable is ", type(price))
+print("Data type of name variable is ", type(name))
