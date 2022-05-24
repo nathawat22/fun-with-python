ecd98dc13b8a4c0b928d59354587c7d7b3ae97e5
Laxman Nathawat <nathawatls@ornl.gov>
2022-05-24 10:56:00 -0400

    added a test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,5 +1,10 @@
 import pytest
+import math
 from src.basics import area
 
 def test_area_rect():
   assert area.area_rect(3,4) == 12
+
+
+def test_area_circle():
+  assert "{:.2f}".format(area.area_circle(4)) == 12.56
